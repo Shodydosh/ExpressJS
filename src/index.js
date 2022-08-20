7896be4be0da5d06d6dd721f918bddaaf636726e
Wh0 <skyzed0@gmail.com>
2022-08-20 12:22:14 +0700

    Update
    - adding template engine (handlebars)
    - restructuring project file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const  { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//! HTTP logger
+app.use(morgan('combined'));
+
+//! Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs'); // set view engine = handlebars
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log(__dirname);
+
+app.get('/', (req, res) => {
+  return res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
